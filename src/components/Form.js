4948ba5e28efd5684def9eeb254b68529f22df29
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-10-16 16:16:15 -0400

    added a file named Form js and started to build out the structure for the form accepting a name email and role function the role function will be a drop down menu the first two will be text

---
+++ src/components/Form.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const Form = props => {
+    const [members, setMembers] = useState({
+      name: '',
+      email: '',
+      role: ''
+    });
+    const submitForm = event => {
+        event.preventDefault();
+        props.addNewMember(members);
+        setMembers({ name: '', email: '', role: ''})
+    }
+    return (
+        <form onSubmit={submitForm}>
+            <label htmlFor='name'>Name</label>
+            <input 
+            name='name'
+            id='name'
+            type='text'
+            placeholder='Name'
+            />
+            <label htmlFor='email'>Email</label>
+            <input 
+            name='email'
+            id='email'
+            type='text'
+            placeholder='Email'
+            />
+            <label htmlFor='role'>Role</label>
+            <select name="role">
+                <option selected value="1">Role</option>
+                <option value="2">Second Value</option>
+                <option value="3">Third Value</option>
+            </select>
+            <button type="submit">Add Member</button>
+
+        </form>
+    );
+}
+
+export default Form;
